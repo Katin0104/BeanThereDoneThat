f2f126505c6eea4e109ec43713d0ea6772d8c81d
Avenel <martin.briewig@googlemail.com>
2024-06-15 18:05:12 +0200

    centers content

--- btdt-app/src/routes/+page.svelte
+++ btdt-app/src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<input type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs" />
-<button class="btn btn-primary">Hello daisyUI</button>
+<div class="mx-auto prose">
+	<h1>Welcome to SvelteKit</h1>
+	<div class="mx-auto prose">
+		<h2>Aktuelle Nachrichten vom {new Date().toLocaleDateString()}</h2>
+	</div>
+	<input
+		type="text"
+		placeholder="Type here"
+		class="input input-bordered input-primary w-full max-w-xs"
+	/>
+	<button class="btn btn-primary">Hello daisyUI</button>
+</div>
