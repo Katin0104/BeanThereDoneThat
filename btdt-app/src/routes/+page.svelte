cb84c409a584ebb8420cf420f6deb1433522d229
Karin Winkler <karin.w@outlook.de>
2024-06-15 18:27:21 +0200

    text to console

--- btdt-app/src/routes/+page.svelte
+++ btdt-app/src/routes/+page.svelte
@@ -1,9 +1,19 @@
+<script lang="ts">
+    let coffeerecipe : string = "";
+    function brew() {
+        console.log("hello world", coffeerecipe);
+    }
+</script>
+
 <div class="mx-auto prose">
 	<h1>Wie möchten Sie Ihren Kaffee heute genießen?</h1>
 	<input
 		type="text"
 		placeholder="Type here"
 		class="input input-bordered input-primary w-full max-w-xs"
+        bind:value={coffeerecipe}
 	/>
-	<button class="btn btn-primary">Kaffee brühen</button>
+	<button class="btn btn-primary" on:click={brew}>Kaffee brühen</button>
 </div>
+
+
