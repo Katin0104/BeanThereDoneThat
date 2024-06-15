853834856a97c43c394c642ca8c1252fc07e9dba
Karin Winkler <karin.w@outlook.de>
2024-06-15 18:11:05 +0200

    unnötig

--- btdt-app/src/routes/+page.svelte
+++ btdt-app/src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <div class="mx-auto prose">
 	<h1>Welcome to SvelteKit</h1>
-	<div class="mx-auto prose">
-		<h2>Aktuelle Nachrichten vom {new Date().toLocaleDateString()}</h2>
-	</div>
 	<input
 		type="text"
 		placeholder="Type here"
