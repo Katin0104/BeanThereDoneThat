95403002f867dd98fa482cb46af7acba6eeb61f4
Karin Winkler <karin.w@outlook.de>
2024-06-16 17:34:07 +0200

    adds add roast house form

---
+++ btdt-app/src/routes/roast-houses/create/+page.svelte
@@ -0,0 +1,120 @@
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<form>
+	<div class="space-y-12">
+		<div class="border-b border-gray-900/10 pb-12">
+			<h2 class="text-base font-semibold leading-7 text-gray-900">Füge eine neue Rösterei hinzu</h2>
+			<p class="mt-1 text-sm leading-6 text-gray-600">
+				Füge möglichst aktuelle Informationen über deine Lieblingsrösterei hinzu.
+			</p>
+
+			<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+				<div class="sm:col-span-3">
+					<label for="first-name" class="block text-sm font-medium leading-6 text-gray-900"
+						>Name der Rösterei</label
+					>
+					<div class="mt-2">
+						<input
+							type="text"
+							name="name"
+							id="name"
+							autocomplete="given-name"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+				<div class="sm:col-span-4">
+					<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+						>Email address</label
+					>
+					<div class="mt-2">
+						<input
+							id="email"
+							name="email"
+							type="email"
+							autocomplete="email"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+				<div class="sm:col-span-4">
+					<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+						>Telefonnumer</label
+					>
+					<div class="mt-2">
+						<input
+							id="tel"
+							name="tel"
+							type="tel"
+							autocomplete="tel"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+
+				<div class="sm:col-span-3">
+					<label for="country" class="block text-sm font-medium leading-6 text-gray-900"
+						>Adresse</label
+					>
+					<div class="mt-2">
+						<input
+							id="address"
+							name="address"
+							type="address"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+				<div class="sm:col-span-3">
+					<label for="country" class="block text-sm font-medium leading-6 text-gray-900"
+						>Link zur Webseite</label
+					>
+					<div class="mt-2">
+						<input
+							id="website"
+							name="website"
+							type="website"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+				<div class="sm:col-span-3">
+					<label for="country" class="block text-sm font-medium leading-6 text-gray-900"
+						>Link zum Shop</label
+					>
+					<div class="mt-2">
+						<input
+							id="shopUrl"
+							name="shopUrl"
+							type="shopUrl"
+							class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="mt-6 flex items-center justify-end gap-x-6">
+		<button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+		<button
+			type="submit"
+			class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+			>Save</button
+		>
+	</div>
+</form>
