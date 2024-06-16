423c811bff2348e09cf53b5eb1369ff0ab6264c0
Karin Winkler <karin.w@outlook.de>
2024-06-16 12:27:01 +0200

    docker compose

--- btdt-app/src/db/data-source.ts
+++ btdt-app/src/db/data-source.ts
@@ -5,11 +5,11 @@ export class BtdtDataSource {
     public get dataSourceInstance() {
         return BtdtDataSource._dataSourceInstance ? BtdtDataSource._dataSourceInstance : new DataSource({
             type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "user",
-            password: "password",
-            database: "btdt",
+            host: process.env.POSTGRES_HOST ?? 'localhost',
+            port: (process.env.POSTGRES_PORT ?? 5434) as number,
+            username: process.env.POSTGRES_USER ?? 'user',
+            password: process.env.POSTGRES_PASSWORD ?? 'password',
+            database: process.env.POSTGRES_DB ?? 'btdt',
             synchronize: true,
             logging: false,
             entities: []
