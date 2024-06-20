62daa71bd462b6c0ca313df36610a53e493e5cba
Karin Winkler <karin.w@outlook.de>
2024-06-20 17:22:12 +0200

    remoddeling add roasting house page

--- btdt-app/src/routes/roast-houses/create/+page.svelte
+++ btdt-app/src/routes/roast-houses/create/+page.svelte
@@ -1,73 +1,49 @@
 <form method="POST">
-	<div class="space-y-12 prose">
-		<div class="border-b border-gray-900/10 pb-12">
-			<h2 class="text-base font-semibold leading-7">Füge eine neue Rösterei hinzu</h2>
-			<p class="mt-1 text-sm leading-6">
-				Füge möglichst aktuelle Informationen über deine Lieblingsrösterei hinzu.
-			</p>
+	<div class="prose">
+		<h2>Füge eine neue Rösterei hinzu</h2>
+		<p>Füge möglichst aktuelle Informationen über deine Lieblingsrösterei hinzu.</p>
 
-			<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-				<div class="sm:col-span-3">
-					<label for="first-name" class="block text-sm font-medium leading-6"
-						>Name der Rösterei</label
-					>
-					<div class="mt-2">
-						<input
-							type="text"
-							name="name"
-							id="name"
-							autocomplete="given-name"
-							class="input input-bordered w-full"
-						/>
-					</div>
-				</div>
-
-				<div class="sm:col-span-4">
-					<label for="email" class="block text-sm font-medium leading-6">Email address</label>
-					<div class="mt-2">
-						<input
-							id="email"
-							name="email"
-							type="email"
-							autocomplete="email"
-							class="input input-bordered"
-						/>
-					</div>
-				</div>
-
-				<div class="sm:col-span-4">
-					<label for="email" class="block text-sm font-medium leading-6">Telefonnumer</label>
-					<div class="mt-2">
-						<input
-							id="tel"
-							name="tel"
-							type="text"
-							autocomplete="tel"
-							class="input input-bordered"
-						/>
-					</div>
-				</div>
-
-				<div class="sm:col-span-3">
-					<label for="country" class="block text-sm font-medium leading-6">Adresse</label>
-					<div class="mt-2">
-						<input id="address" name="address" type="text" class="input input-bordered" />
-					</div>
-				</div>
-				<div class="sm:col-span-3">
-					<label for="country" class="block text-sm font-medium leading-6">Link zur Webseite</label>
-					<div class="mt-2">
-						<input id="website" name="website" type="text" class="input input-bordered" />
-					</div>
-				</div>
-				<div class="sm:col-span-3">
-					<label for="country" class="block text-sm font-medium leading-6">Link zum Shop</label>
-					<div class="mt-2">
-						<input id="shopUrl" name="shopUrl" type="text" class="input input-bordered" />
-					</div>
-				</div>
+		<label for="first-name" class="form-control text-sm font-medium mt-2">
+			<div class="label">
+				<span class="label-text">Name der Rösterei</span>
 			</div>
-		</div>
+			<input type="text" name="name" id="name" class="input input-bordered w-full" /></label
+		>
+
+		<label for="email" class="form-control block text-sm font-medium mt-3">
+			<div class="label">
+				<span class="label-text">Email Addresse</span>
+			</div>
+			<input id="email" name="email" type="email" class="input input-bordered w-full" />
+		</label>
+
+		<label for="tel" class="form-control block text-sm font-medium mt-2">
+			<div class="label">
+				<span class="label-text">Telefonnumer</span>
+			</div>
+			<input id="tel" name="tel" type="text" class="input input-bordered w-full" /></label
+		>
+
+		<label for="address" class="form-control block text-sm font-medium mt-2">
+			<div class="label">
+				<span class="label-text">Adresse</span>
+			</div>
+			<input id="address" name="address" type="text" class="input input-bordered w-full" />
+		</label>
+
+		<label for="website" class="form-control block text-sm font-medium mt-2">
+			<div class="label">
+				<span class="label-text">Link zur Webseite</span>
+			</div>
+			<input id="website" name="website" type="text" class="input input-bordered w-full" />
+		</label>
+
+		<label for="shopUrl" class="form-control font-medium w-full mt-2">
+			<div class="label">
+				<span class="label-text">Link zum Shop</span>
+			</div>
+			<input id="shopUrl" name="shopUrl" type="text" class="input input-bordered w-full" />
+		</label>
 	</div>
 
 	<div class="mt-6 flex items-center justify-end gap-x-6">
