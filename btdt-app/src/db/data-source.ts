965227f7019e7dac0365fa72b5fbd05ae8a057ba
Avenel <martin.briewig@googlemail.com>
2024-09-08 13:14:04 +0200

    enables ssl for postgres

--- btdt-app/src/db/data-source.ts
+++ btdt-app/src/db/data-source.ts
@@ -1,19 +1,32 @@
 import { DataSource } from "typeorm"
 import { RoastingHouse } from "./entities/roasting-house";
 import { CoffeeBeans } from "./entities/coffee-beans";
+import { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_USE_SSL } from "$env/static/private";
 
 export class BtdtDataSource {
     private static _dataSourceInstance: DataSource;
     public get dataSourceInstance() {
+        // console.log(process.env);
+        const dbConfig = {
+            type: "postgres",
+            host: POSTGRES_HOST ?? 'localhost',
+            port: Number.parseInt(POSTGRES_PORT ?? '5434'),
+            username: POSTGRES_USER ?? 'user',
+            password: POSTGRES_PASSWORD ?? 'password',
+            database: POSTGRES_DB ?? 'btdt',
+            ssl: POSTGRES_USE_SSL === 'true'
+        }
+        console.log(dbConfig)
         BtdtDataSource._dataSourceInstance = BtdtDataSource._dataSourceInstance ? BtdtDataSource._dataSourceInstance : new DataSource({
             type: "postgres",
-            host: process.env.POSTGRES_HOST ?? 'localhost',
-            port: (process.env.POSTGRES_PORT ?? 5434) as number,
-            username: process.env.POSTGRES_USER ?? 'user',
-            password: process.env.POSTGRES_PASSWORD ?? 'password',
-            database: process.env.POSTGRES_DB ?? 'btdt',
+            host: dbConfig.host,
+            port: dbConfig.port,
+            username: dbConfig.username,
+            password: dbConfig.password,
+            database: dbConfig.database,
             synchronize: true,
             logging: false,
+            ssl: dbConfig.ssl,
             entities: [RoastingHouse, CoffeeBeans]
         })
 
